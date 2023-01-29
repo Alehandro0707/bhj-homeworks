cb9eada6a42f724b9498db1be031f10322fb4d1e
Alehandro0707 <zheltov007@yandex.ru>
2023-01-29 18:10:56 +0300

    keysoloupdatetask

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,23 +17,21 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+
+    document.addEventListener('keydown', (evt) => {
+      let char = this.currentSymbol.textContent;
+        if (evt.key === char) {
+          this.success();
+        } else {
+          this.fail();
+        }
+    });
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
-
     if (this.currentSymbol !== null) {
-      this.currentSymbol.classList.add('symbol_current');
       return;
     }
 
@@ -90,5 +88,4 @@ class Game {
   }
 }
 
-new Game(document.getElementById('game'))
-
+new Game(document.getElementById('game'));
